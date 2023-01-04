d1fe5cf877b6f3a591c63183ac27e58f4ab27167
DESKTOP-H3M6PSM\khs55 <omfg6424@gmail.com>
2023-01-04 20:50:30 +0900

    no message

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.set("port", process.env.PORT || 3000);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.engine('ejs', require('ejs').__express);
 
 sequelize
   .sync({ force: false })
