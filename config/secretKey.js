d9a11f747f443e0e0f692df63779ec63dd6bb3cf
minzunim <greenteapig6@gmail.com>
2023-01-06 17:44:45 +0900

    no message

---
+++ config/secretKey.js
@@ -0,0 +1,9 @@
+module.exports = {
+  secretKey: "YoUrSeCrEtKeY", // 원하는 시크릿 키
+  secretKey2: "MySeCrEtKeY",
+  option: {
+    algorithm: "HS256", // 해싱 알고리즘
+    expiresIn: "30m", // 토큰 유효 기간
+    issuer: "issuer", // 발행자
+  },
+};
