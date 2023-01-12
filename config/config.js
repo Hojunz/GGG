a12b6c5728d3cccfc86fda814e44fbd8a95065aa
minzunim <118159081+minzunim@users.noreply.github.com>
2023-01-12 16:14:47 +0900

    config.js 추가

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_ID,
+    password: process.env.DB_PASSWORD,
+    database: "ggg",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    logging: false,
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
